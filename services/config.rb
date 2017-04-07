4c58eb193837f907a0f6effb31498ea80337a7b3
Michael Pautov <michael.pautov@gmail.com>
2017-04-07 11:12:32 +0600

    Changed planwide.number_violations to GLOBAL.number-violations

--- services/config.rb
+++ services/config.rb
@@ -802,7 +802,7 @@ coreo_uni_util_variables "iam-planwide" do
                 {'COMPOSITE::coreo_uni_util_variables.iam-planwide.composite_name' => 'PLAN::stack_name'},
                 {'COMPOSITE::coreo_uni_util_variables.iam-planwide.plan_name' => 'PLAN::name'},
                 {'COMPOSITE::coreo_uni_util_variables.iam-planwide.results' => 'unset'},
-                {'COMPOSITE::coreo_uni_util_variables.iam-planwide.number_violations' => '0'}
+                {'GLOBAL::number_violations' => '0'}
             ])
 end
 
@@ -816,7 +816,7 @@ coreo_uni_util_variables "iam-update-planwide-1" do
   action :set
   variables([
                 {'COMPOSITE::coreo_uni_util_variables.iam-planwide.results' => 'COMPOSITE::coreo_aws_rule_runner.advise-iam.report'},
-                {'COMPOSITE::coreo_uni_util_variables.iam-planwide.number_violations' => 'COMPOSITE::coreo_aws_rule_runner.advise-iam.number_violations'},
+                {'GLOBAL::number_violations' => 'COMPOSITE::coreo_aws_rule_runner.advise-iam.number_violations'},
 
             ])
 end
