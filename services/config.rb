3fa155fb002a4d66cb1d7ad3f53d92564cf27abd
Michael Pautov <michael.pautov@gmail.com>
2017-04-07 11:41:23 +0600

    set npm-common module version 1.9.7-beta7

--- services/config.rb
+++ services/config.rb
@@ -985,7 +985,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-9"
+                   :version => "1.9.7-beta7"
                },
                {
                    :name => "js-yaml",
