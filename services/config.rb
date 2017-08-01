ffd0c762d2a82ebdbc0ee46dd814f29011227964
Paul Allen <pauldallen@gmail.com>
2017-08-01 01:44:10 -0700

    CON-596 use regex instead of ==

--- services/config.rb
+++ services/config.rb
@@ -867,7 +867,8 @@ coreo_aws_rule_runner "advise-iam" do
   service :iam
   action :run
   regions ["PLAN::region"]
-  rules ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-internal", "iam-policy-internal")
+  #rules ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-internal", "iam-policy-internal")
+  rules ${AUDIT_AWS_IAM_ALERT_LIST}
   id_map ["modifiers.user_name", "static.password_policy"]
   filter(${FILTERED_OBJECTS}) if ${FILTERED_OBJECTS}
 end
