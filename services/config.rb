da9e50d5f5e57ba59c67cc44764777ccc0806d76
Paul Allen <pauldallen@gmail.com>
2017-09-06 18:06:16 -0700

    CON-632 fixing pushed vars

--- services/config.rb
+++ services/config.rb
@@ -883,25 +883,12 @@ coreo_uni_util_variables "iam-planwide" do
             ])
 end
 
-coreo_uni_util_variables "iam-runner-rules" do
-  action :set
-  variables([
-                {'COMPOSITE::coreo_uni_util_variables.iam-runner-rules.rules' => ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-internal", "iam-policy-internal")}
-            ])
-end
-
-coreo_uni_util_variables "iam-runner-rules-admin" do
-  action((${AUDIT_AWS_IAM_ALERT_LIST}.include?('iam-user-is-admin') ? :set : :nothing))
-  variables([
-                {'COMPOSITE::coreo_uni_util_variables.iam-runner-rules.rules' => ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-inventory-users", "iam-inventory-roles", "iam-inventory-policies", "iam-inventory-groups")}
-            ])
-end
-
 coreo_aws_rule_runner "advise-iam" do
   service :iam
   action :run
   regions ["PLAN::region"]
-  rules([].push(eval('COMPOSITE::coreo_uni_util_variables.iam-runner-rules.rules')))
+  rules ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-internal", "iam-policy-internal")}
+  rules ${AUDIT_AWS_IAM_ALERT_LIST}.push("iam-internal", "iam-policy-internal")}.push("iam-inventory-users", "iam-inventory-roles", "iam-inventory-policies", "iam-inventory-groups")} if ${AUDIT_AWS_IAM_ALERT_LIST}.include?('iam-user-is-admin')
   filter(${FILTERED_OBJECTS}) if ${FILTERED_OBJECTS}
 end
 
