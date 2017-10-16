0d61e2a1fd737ba57d11084ca8aeeefc28ab6278
omurbek <omurbek.jk@gmail.com>
2017-10-16 10:32:14 +0600

    PLA-4917 updated jsrunner to 1.10.7-30

--- services/config.rb
+++ services/config.rb
@@ -1408,7 +1408,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-iam" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-28"
+                   :version => "1.10.7-30"
                },
                {
                    :name => "js-yaml",
